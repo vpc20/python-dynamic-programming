c2d4fe9d7b4660b216d2fbd197fa810a7140b180
vpc <cuevasvp@gmail.com>
2020-04-09 05:58:45 +0800

    refactor to extract numrow, numcols

--- MinPathSum.py
+++ MinPathSum.py
@@ -12,18 +12,21 @@
 # Output: 7
 # Explanation: Because the path 1→3→1→1→1 minimizes the sum.
 
+
 # def min_path_sum(arr):
 #     # dp_arr = [[0] * (len(arr[0])) for _ in range(len(arr))]
-#     dp_arr = [[0 for _ in range(len(arr[0]))] for _ in range(len(arr))]
+#     numrows = len(arr)
+#     numcols = len(arr[0])
+#     dp_arr = [[0 for _ in range(numcols)] for _ in range(numrows)]
 #
 #     dp_arr[0][0] = arr[0][0]
-#     for j in range(1, len(arr[0])): # init dp_arr first row
+#     for j in range(1, numcols):  # init dp_arr first row
 #         dp_arr[0][j] = arr[0][j] + dp_arr[0][j - 1]
-#     for i in range(1, len(arr)): # init dp_arr first col
+#     for i in range(1, numrows):  # init dp_arr first col
 #         dp_arr[i][0] = arr[i][0] + dp_arr[i - 1][0]
 #
-#     for i in range(1, len(arr)):
-#         for j in range(1, len(arr[0])):
+#     for i in range(1, numrows):
+#         for j in range(1, numcols):
 #             dp_arr[i][j] = min(arr[i][j] + dp_arr[i][j - 1], arr[i][j] + dp_arr[i - 1][j])
 #     for e in dp_arr:
 #         print(e)
@@ -46,6 +49,23 @@ def min_path_sum(grid):
     for e in dp:
         print(e)
     return dp[-1][-1]
+
+
+# def min_path_sum(grid):  # faster but will change the input array (grid)
+#     numrows = len(grid)
+#     numcols = len(grid[0])
+#
+#     for j in range(1, numcols):  # init dp first row
+#         grid[0][j] += grid[0][j - 1]
+#     for i in range(1, numrows):  # init dp first col
+#         grid[i][0] += grid[i - 1][0]
+#
+#     for i in range(1, numrows):
+#         for j in range(1, numcols):
+#             grid[i][j] = min(grid[i][j] + grid[i][j - 1], grid[i][j] + grid[i - 1][j])
+#     for e in grid:
+#         print(e)
+#     return grid[-1][-1]
 
 
 # def min_path_sum(arr, i, j):
