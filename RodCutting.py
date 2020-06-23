cdc2e79db4ea2db115ffd077e8b4a2c421e364b6
vpc <cuevasvp@gmail.com>
2020-06-24 07:17:43 +0800

    refactor code

--- RodCutting.py
+++ RodCutting.py
@@ -21,20 +21,30 @@ import sys
 from itertools import combinations_with_replacement
 
 
-def cut_rod_naive(prices, rod_len):
-    max_price = 0
-    max_comb = ()
-    for r in range(1, rod_len + 1):
-        for comb in combinations_with_replacement([i for i in range(1, len(prices) + 1)], r):
-            if sum(comb) == rod_len:
-                total_price = 0
-                for i in comb:
-                    total_price += prices[i - 1]
-                if total_price > max_price:
-                    # print(comb, total_price)
-                    max_price = total_price
-                    max_comb = comb
-    return max_price  # , max_comb
+# def cut_rod_naive(prices, rod_len):
+#     maxprc = 0
+#     maxcomb = ()
+#     lengths = [i for i in range(1, len(prices) + 1)]
+#     for r in range(1, rod_len + 1):
+#         for comb in combinations_with_replacement(lengths, r):
+#             if sum(comb) == rod_len:
+#                 total_price = sum([prices[i - 1] for i in comb])
+#                 if total_price > maxprc:
+#                     # print(comb, total_price)
+#                     maxprc = total_price
+#                     maxcomb = comb
+#     return maxprc  # , maxcomb
+
+
+def cut_rod_naive(prices, l):
+    maxprc = 0
+    lengths = [i for i in range(1, len(prices) + 1)]
+    for r in range(1, l + 1):
+        for comb in combinations_with_replacement(lengths, r):
+            if sum(comb) == l:
+                totprc = sum([prices[i - 1] for i in comb])
+                maxprc = max(maxprc, totprc)
+    return maxprc
 
 
 # def cut_rod_recur(prices, rod_len):
@@ -47,21 +57,21 @@ def cut_rod_naive(prices, rod_len):
 
 
 def cut_rod_recur(prices, rod_len):
-    def _cut_rod_recur(prices, rod_len):
-        nonlocal max_price
+    def cut_rod(prices, rod_len):
+        nonlocal maxprc
         if not prices or rod_len == 0:
             return 0
         if rod_len >= len(prices):
-            tot_price = max(_cut_rod_recur(prices[:-1], rod_len),
-                            prices[-1] + _cut_rod_recur(prices, rod_len - len(prices)))
+            totprc = max(cut_rod(prices[:-1], rod_len),
+                         prices[-1] + cut_rod(prices, rod_len - len(prices)))
         else:
-            tot_price = _cut_rod_recur(prices[:-1], rod_len)
-        max_price = max(max_price, tot_price)
-        return tot_price
+            totprc = cut_rod(prices[:-1], rod_len)
+        maxprc = max(maxprc, totprc)
+        return totprc
 
-    max_price = -sys.maxsize
-    _cut_rod_recur(prices, rod_len)
-    return max_price
+    maxprc = -sys.maxsize
+    cut_rod(prices, rod_len)
+    return maxprc
 
 
 # def cut_rod_recur_dyna(prices, rod_len):
@@ -81,24 +91,6 @@ def cut_rod_recur(prices, rod_len):
 #     return _cut_rod_recur_dyna(prices, rod_len)
 
 
-# def cut_rod_recur(prices, rod_len): #incorrect
-#     # max_price = 0
-#     max_set = []
-#
-#     def _cut_rod_recur(prices, rod_len):
-#         # print(prices, rod_len)
-#         if rod_len == 0:
-#             return 0
-#         max_price = -sys.maxsize
-#         for i in range(1, rod_len + 1):
-#             comp_price = prices[i - 1] + _cut_rod_recur(prices, rod_len - i)
-#             if comp_price > max_price:
-#                 max_price = comp_price
-#                 max_set.append(i)
-#         return max_price
-#
-#     return _cut_rod_recur(prices, rod_len), max_set
-
 # def cut_rod_dyna(prices, rod_len): # cormen book solution
 #     dp_arr = [0] * (rod_len + 1)
 #     # print(dp_arr)
@@ -111,83 +103,84 @@ def cut_rod_recur(prices, rod_len):
 #     return dp_arr[rod_len]
 
 
-def cut_rod_dyna(prices, rod_len):
-    dp_arr = [[0] * (rod_len + 1) for _ in range(len(prices) + 1)]
-    max_price = -sys.maxsize
+def cut_rod_dyna(prices, l):
+    dp = [[0] * (l + 1) for _ in range(len(prices) + 1)]
+    maxprc = -sys.maxsize
     for i in range(1, len(prices) + 1):
-        for j in range(1, rod_len + 1):
+        for j in range(1, l + 1):
             if j >= i:
-                dp_arr[i][j] = max(dp_arr[i - 1][j], prices[i - 1] + dp_arr[i][j - i])
+                dp[i][j] = max(dp[i - 1][j], prices[i - 1] + dp[i][j - i])
             else:
-                dp_arr[i][j] = dp_arr[i - 1][j]
-            max_price = max(max_price, dp_arr[i][j])
-    return max_price
+                dp[i][j] = dp[i - 1][j]
+            maxprc = max(maxprc, dp[i][j])
+    print(dp)
+    return maxprc
 
 
-# def cut_rod_dyna(prices, rod_len):  # simplified version
-#     dp_arr = [0] * (rod_len + 1)
-#     # print(dp_arr)
-#     max_price = -sys.maxsize
+# def cut_rod_dyna(prices, l):  # simplified version
+#     dp = [0] * (l + 1)
+#     maxprc = -sys.maxsize
 #     for i in range(1, len(prices) + 1):
-#         for j in range(1, rod_len + 1):
+#         for j in range(1, l + 1):
 #             if j >= i:
-#                 dp_arr[j] = max(dp_arr[j], prices[i - 1] + dp_arr[j - i])
-#             max_price = max(max_price, dp_arr[j])
-#         # print(dp_arr)
-#     return max_price
+#                 dp[j] = max(dp[j], prices[i - 1] + dp[j - i])
+#             maxprc = max(maxprc, dp[j])
+#     return maxprc
 
 
 if __name__ == '__main__':
-    # prices_arr = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+    prices = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
 
-    # for i in range(1, 11):
-    #     print(cut_rod_naive(prices_arr, i))
-    #     print(cut_rod_recur(prices_arr, i))
-    #     print(cut_rod_recur_dyna(prices_arr, i))
-    #     print(cut_rod_dyna(prices_arr, i))
+    # assert cut_rod_naive(prices, 1) == cut_rod_recur(prices, 1)
+    # assert cut_rod_naive(prices, 2) == cut_rod_recur(prices, 2)
+    # assert cut_rod_naive(prices, 3) == cut_rod_recur(prices, 3)
+    # assert cut_rod_naive(prices, 4) == cut_rod_recur(prices, 4)
+    # assert cut_rod_naive(prices, 5) == cut_rod_recur(prices, 5)
+    # assert cut_rod_naive(prices, 6) == cut_rod_recur(prices, 6)
+    # assert cut_rod_naive(prices, 7) == cut_rod_recur(prices, 7)
+    # assert cut_rod_naive(prices, 8) == cut_rod_recur(prices, 8)
+    #
+    # assert cut_rod_naive(prices, 1) == cut_rod_dyna(prices, 1)
+    # assert cut_rod_naive(prices, 2) == cut_rod_dyna(prices, 2)
+    # assert cut_rod_naive(prices, 3) == cut_rod_dyna(prices, 3)
+    # assert cut_rod_naive(prices, 4) == cut_rod_dyna(prices, 4)
+    # assert cut_rod_naive(prices, 5) == cut_rod_dyna(prices, 5)
+    # assert cut_rod_naive(prices, 6) == cut_rod_dyna(prices, 6)
+    # assert cut_rod_naive(prices, 7) == cut_rod_dyna(prices, 7)
+    # assert cut_rod_naive(prices, 8) == cut_rod_dyna(prices, 8)
 
-    # print(cut_rod_naive(prices_arr, 20))
-    # print(cut_rod_recur(prices_arr, 4))
+    print(cut_rod_dyna(prices, 4))
 
-    prices_arr = [1, 5, 8]
+#      0   1   2   3    4
+#    ┌───┬───┬───┬───┬────┐
+#  0 │ 0 │ 0 │ 0 │ 0 │  0 │
+#    ├───┼───┼───┼───┼────┤
+#  1 │ 0 │ 1 │ 2 │ 3 │  4 │
+#    ├───┼───┼───┼───┼────┤
+#  2 │ 0 │ 1 │ 5 │ 6 │ 10 │
+#    ├───┼───┼───┼───┼────┤
+#  3 │ 0 │ 1 │ 5 │ 8 │ 10 │
+#    ├───┼───┼───┼───┼────┤
+#  4 │ 0 │ 1 │ 5 │ 8 │ 10 │
+#    ├───┼───┼───┼───┼────┤
+#  5 │ 0 │ 1 │ 5 │ 8 │ 10 │
+#    ├───┼───┼───┼───┼────┤
+#  6 │ 0 │ 1 │ 5 │ 8 │ 10 │
+#    ├───┼───┼───┼───┼────┤
+#  7 │ 0 │ 1 │ 5 │ 8 │ 10 │
+#    ├───┼───┼───┼───┼────┤
+#  8 │ 0 │ 1 │ 5 │ 8 │ 10 │
+#    ├───┼───┼───┼───┼────┤
+#  9 │ 0 │ 1 │ 5 │ 8 │ 10 │
+#    ├───┼───┼───┼───┼────┤
+# 10 │ 0 │ 1 │ 5 │ 8 │ 10 │
+#    └───┴───┴───┴───┴────┘
 
-    # print(cut_rod_naive(prices_arr, 1))
-    # print(cut_rod_naive(prices_arr, 2))
-    # print(cut_rod_naive(prices_arr, 3))
-    # print(cut_rod_naive(prices_arr, 4))
-    # print(cut_rod_naive(prices_arr, 5))
-    # print(cut_rod_naive(prices_arr, 6))
-    # print(cut_rod_naive(prices_arr, 7))
-    # print(cut_rod_naive(prices_arr, 8))
 
-    # print(cut_rod_dyna(prices_arr, 8))
-
-    print(cut_rod_recur(prices_arr, 1))
-    print(cut_rod_recur(prices_arr, 2))
-    print(cut_rod_recur(prices_arr, 3))
-    print(cut_rod_recur(prices_arr, 4))
-    print(cut_rod_recur(prices_arr, 5))
-    print(cut_rod_recur(prices_arr, 6))
-    print(cut_rod_recur(prices_arr, 7))
-    print(cut_rod_recur(prices_arr, 8))
-
-    #      0    1    2    3    4
-    #   ┌────┬────┬────┬────┬────┐
-    # ø │  0 │  0 │  0 │  0 │  0 │
-    #   ├────┼────┼────┼────┼────┤
-    # 1 │  0 │  1 │  0 │  0 │  0 │
-    #   ├────┼────┼────┼────┼────┤
-    # 2 │  0 │  1 │  5 │  0 │  0 │
-    #   ├────┼────┼────┼────┼────┤
-    # 3 │  0 │  1 │  5 │  8 │  0 │
-    #   ├────┼────┼────┼────┼────┤
-    # 4 │  0 │  1 │  5 │  8 │ 10 │
-    #   └────┴────┴────┴────┴────┘
-
-    prices_arr = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24,  # 0X's
-                  30, 32, 35, 39, 43, 43, 45, 49, 50, 54,  # 1X's
-                  57, 60, 65, 68, 70, 74, 80, 81, 84, 85,  # 2X's
-                  87, 91, 95, 99, 101, 104, 107, 112, 115, 116,  # 3X's
-                  119]  # 40th element
-
-    print(cut_rod_dyna(prices_arr, 1))
+# prices_arr = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24,  # 0X's
+#               30, 32, 35, 39, 43, 43, 45, 49, 50, 54,  # 1X's
+#               57, 60, 65, 68, 70, 74, 80, 81, 84, 85,  # 2X's
+#               87, 91, 95, 99, 101, 104, 107, 112, 115, 116,  # 3X's
+#               119]  # 40th element
+#
+# print(cut_rod_dyna(prices_arr, 1))
